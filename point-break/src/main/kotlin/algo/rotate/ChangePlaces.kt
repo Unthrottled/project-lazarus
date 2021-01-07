0ebd7bbbb43228dae60e1d76363d0171110b429f
DATBOI <alexsimons9999@gmail.com>
2021-01-07 04:43:55 -0600

    Can rotate left

--- point-break/src/main/kotlin/algo/rotate/ChangePlaces.kt
+++ point-break/src/main/kotlin/algo/rotate/ChangePlaces.kt
@@ -4,20 +4,27 @@ fun main() {
   val toShift = readLine()!!
   repeat(toShift.length * 2) {
     val shift = it + 1
-    println("Shifting '$toShift' $shift times ${rotateString(toShift, shift)}")
+    println("Shifting right '$toShift' $shift times ${rotateString(toShift, shift, Direction.RIGHT)}")
+    println("Shifting left '$toShift' $shift times ${rotateString(toShift, shift, Direction.LEFT)}")
   }
 }
 
-fun rotateString(
+internal enum class Direction {
+  LEFT, RIGHT
+}
+
+private fun rotateString(
   toShift: String,
-  shiftRight: Int,
+  shiftPlaces: Int,
+  direction: Direction,
 ): String {
   val length = toShift.length
-  val offset = shiftRight % length
+  val offset = shiftPlaces % length
   return if (offset == 0) {
     toShift
   } else {
-    var i = offset
+    var i = if (direction == Direction.RIGHT) offset
+    else length - offset
     val shiftedString = CharArray(length)
     for (j in 0 until length) {
       shiftedString[i] = toShift[j]
@@ -26,3 +33,4 @@ fun rotateString(
     String(shiftedString)
   }
 }
+
